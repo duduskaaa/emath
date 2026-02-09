ba1e9e083d631f4a5f7d47a39674cb6aa8fe994e
duduskaaa <ergalikhatep@gmail.com>
2026-02-09 16:59:01 +0500

    FIX: CrudRepository instead JpaRepository

--- src/main/java/com/khatep/emath/teacher/src/main/java/com/khatep/teacher/repository/TeacherRepository.java
+++ src/main/java/com/khatep/emath/teacher/src/main/java/com/khatep/teacher/repository/TeacherRepository.java
@@ -1,8 +1,8 @@
 package com.khatep.teacher.repository;
 
 import com.khatep.teacher.entity.Teacher;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface TeacherRepository extends CrudRepository<Teacher, Long> {
+public interface TeacherRepository extends JpaRepository<Teacher, Long> {
     Teacher getTeacherById(Long id);
 }
