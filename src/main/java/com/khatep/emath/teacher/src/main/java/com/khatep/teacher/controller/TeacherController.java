6cdf323879d343514ddcacc7f646d31aa553aac3
duduskaaa <ergalikhatep@gmail.com>
2026-02-09 16:55:46 +0500

    ADD: CRUD operations

--- src/main/java/com/khatep/emath/teacher/src/main/java/com/khatep/teacher/controller/TeacherController.java
+++ src/main/java/com/khatep/emath/teacher/src/main/java/com/khatep/teacher/controller/TeacherController.java
@@ -1,8 +1,42 @@
 package com.khatep.teacher.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.khatep.teacher.dto.TeacherRequestDto;
+import com.khatep.teacher.dto.TeacherResponseDto;
+import com.khatep.teacher.service.TeacherService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
+@RequiredArgsConstructor
 public class TeacherController {
+    private final TeacherService teacherService;
 
+    @GetMapping
+    public List<TeacherResponseDto> getAll() {
+        return teacherService.getAll();
+    }
+
+    @GetMapping("/{id}")
+    public TeacherResponseDto getTeacherById(@PathVariable Long id) {
+        return teacherService.getById(id);
+    }
+
+    @PostMapping
+    public void create(@RequestBody TeacherRequestDto dto) {
+        teacherService.create(dto);
+    }
+
+    @PutMapping("/{id}")
+    //TODO: Add update methods for other fields
+    public void update(@PathVariable Long id) {
+        teacherService.update(id);
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable Long id) {
+        teacherService.delete(id);
+
+    }
 }
