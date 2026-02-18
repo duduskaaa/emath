b42a89802874998058fa92ef5748978d2dd322c8
duduskaaa <ergalikhatep@gmail.com>
2026-02-18 15:29:23 +0500

    ADD: updateMapper that ignores null fields

--- src/main/java/com/khatep/emath/teacher/src/main/java/com/khatep/teacher/mapper/TeacherMapper.java
+++ src/main/java/com/khatep/emath/teacher/src/main/java/com/khatep/teacher/mapper/TeacherMapper.java
@@ -3,11 +3,17 @@ package com.khatep.teacher.mapper;
 import com.khatep.teacher.dto.TeacherRequestDto;
 import com.khatep.teacher.dto.TeacherResponseDto;
 import com.khatep.teacher.entity.Teacher;
+import org.mapstruct.BeanMapping;
 import org.mapstruct.Mapper;
+import org.mapstruct.MappingTarget;
+import org.mapstruct.NullValuePropertyMappingStrategy;
 
 @Mapper(componentModel = "spring")
 public interface TeacherMapper {
     TeacherResponseDto toTeacherResponseDto(Teacher teacher);
 
     Teacher toTeacher(TeacherRequestDto teacherRequestDto);
+
+    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+    Teacher updateTeacherFromDto(TeacherRequestDto teacherRequestDto, @MappingTarget Teacher teacher);
 }
