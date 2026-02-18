ff9c2e0fb55e7a6398df048454c449be0e67ef4a
duduskaaa <ergalikhatep@gmail.com>
2026-02-18 14:55:24 +0500

    DELETE: Unnecessary imports

--- src/main/java/com/khatep/emath/teacher/src/main/java/com/khatep/teacher/mapper/TeacherMapper.java
+++ src/main/java/com/khatep/emath/teacher/src/main/java/com/khatep/teacher/mapper/TeacherMapper.java
@@ -4,7 +4,6 @@ import com.khatep.teacher.dto.TeacherRequestDto;
 import com.khatep.teacher.dto.TeacherResponseDto;
 import com.khatep.teacher.entity.Teacher;
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface TeacherMapper {
