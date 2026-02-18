a782da66b7a83d902d19a57ef16246327123950a
duduskaaa <ergalikhatep@gmail.com>
2026-02-18 15:30:45 +0500

    DELETE: getTeacherById and use default findById instead

--- src/main/java/com/khatep/emath/teacher/src/main/java/com/khatep/teacher/repository/TeacherRepository.java
+++ src/main/java/com/khatep/emath/teacher/src/main/java/com/khatep/teacher/repository/TeacherRepository.java
@@ -4,5 +4,4 @@ import com.khatep.teacher.entity.Teacher;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface TeacherRepository extends JpaRepository<Teacher, Long> {
-    Teacher getTeacherById(Long id);
 }
