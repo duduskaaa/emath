f4364aed1afd32186ac75d95d0e0e434ad137d32
duduskaaa <ergalikhatep@gmail.com>
2026-02-18 14:33:09 +0500

    ADD: Getter and Setter

--- src/main/java/com/khatep/emath/teacher/src/main/java/com/khatep/teacher/dto/TeacherResponseDto.java
+++ src/main/java/com/khatep/emath/teacher/src/main/java/com/khatep/teacher/dto/TeacherResponseDto.java
@@ -1,5 +1,10 @@
 package com.khatep.teacher.dto;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class TeacherResponseDto {
     private String firstName;
     private String lastName;
