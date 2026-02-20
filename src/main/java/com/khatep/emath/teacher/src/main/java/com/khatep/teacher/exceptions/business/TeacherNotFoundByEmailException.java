8d198a97ca3bd5993bcf11e9eba528943d52f9b2
duduskaaa <ergalikhatep@gmail.com>
2026-02-20 15:08:37 +0500

    CHANGE: Split methods not found by id and by email

--- src/main/java/com/khatep/emath/teacher/src/main/java/com/khatep/teacher/exceptions/business/TeacherNotFoundByEmailException.java
+++ src/main/java/com/khatep/emath/teacher/src/main/java/com/khatep/teacher/exceptions/business/TeacherNotFoundByEmailException.java
@@ -3,8 +3,8 @@ package com.khatep.teacher.exceptions.business;
 import com.khatep.teacher.exceptions.handler.BaseException;
 import org.springframework.http.HttpStatus;
 
-public class TeacherNotFound extends BaseException {
-    public TeacherNotFound(String message) {
+public class TeacherNotFoundByEmailException extends BaseException {
+    public TeacherNotFoundByEmailException(String message) {
         super(message, HttpStatus.NOT_FOUND, "Teacher not found");
     }
 }
---
+++ src/main/java/com/khatep/emath/teacher/src/main/java/com/khatep/teacher/exceptions/business/TeacherNotFoundByIdException.java
@@ -0,0 +1,10 @@
+package com.khatep.teacher.exceptions.business;
+
+import com.khatep.teacher.exceptions.handler.BaseException;
+import org.springframework.http.HttpStatus;
+
+public class TeacherNotFoundByIdException extends BaseException {
+    public TeacherNotFoundByIdException(String message) {
+        super(message, HttpStatus.NOT_FOUND, "Teacher not found");
+    }
+}
