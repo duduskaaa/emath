36822c522c3eeedff8570d44f0868325e891ebfc
duduskaaa <ergalikhatep@gmail.com>
2026-02-20 14:44:38 +0500

    ADD: GlobalExceptionHandler for log and return exception in JSON format

---
+++ src/main/java/com/khatep/emath/teacher/src/main/java/com/khatep/teacher/exceptions/handler/GlobalExceptionHandler.java
@@ -0,0 +1,29 @@
+package com.khatep.teacher.exceptions.handler;
+
+import com.khatep.teacher.exceptions.ErrorResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.time.LocalDateTime;
+
+@Slf4j
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(BaseException.class)
+    public ResponseEntity<ErrorResponse> buildErrorResponse(BaseException ex) {
+        log.error("Business error {}", ex.getMessage());
+
+        ErrorResponse response = new ErrorResponse(
+                LocalDateTime.now(),
+                ex.getHttpStatus().value(),
+                ex.getErrorTitle(),
+                ex.getMessage()
+        );
+
+        return ResponseEntity
+                .status(ex.getHttpStatus())
+                .body(response);
+    }
+}
