0e6a6c9988c29420913d8720abab4fd2f746ec33
duduskaaa <ergalikhatep@gmail.com>
2026-04-03 16:13:14 +0500

    FIX: Project structure and modules

--- src/main/java/com/khatep/emath/EmathConfiguration.java
+++
@@ -1,9 +0,0 @@
-package com.khatep.emath;
-
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-@ComponentScan(basePackages = {"com.khatep"})
-public class EmathConfiguration {
-}
--- src/main/java/com/khatep/emath/teacher/src/main/java/com/khatep/teacher/configuration/TeacherDataConfiguration.java
+++
@@ -1,8 +0,0 @@
-package com.khatep.teacher.configuration;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.data.web.config.EnableSpringDataWebSupport;
-
-@Configuration
-@EnableSpringDataWebSupport
-public class TeacherDataConfiguration {
-}
