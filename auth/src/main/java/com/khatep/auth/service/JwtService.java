a8812c48105ac2915ead49eb67239bdc85ee8e31
duduskaaa <ergalikhatep@gmail.com>
2026-04-10 15:39:53 +0500

    CREATE: JWT module with generate token

---
+++ auth/src/main/java/com/khatep/auth/service/JwtService.java
@@ -0,0 +1,15 @@
+package com.khatep.auth.service;
+
+import com.khatep.auth.entity.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+public interface JwtService {
+    String generateToken(User user);
+
+    UUID extractUserId(String token);
+
+    boolean isTokenValid(String token, UserDetails userDetails);
+}
---
+++ auth/src/main/java/com/khatep/auth/service/impl/JwtServiceImpl.java
@@ -0,0 +1,54 @@
+package com.khatep.auth.service.impl;
+
+import com.khatep.auth.entity.User;
+import com.khatep.auth.service.JwtService;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
+
+import javax.crypto.SecretKey;
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+import java.util.Date;
+import java.util.UUID;
+
+@Service
+public class JwtServiceImpl implements JwtService {
+    @Value("${jwt.secret}")
+    private String secret;
+
+    private SecretKey secretKey;
+
+    @Override
+    public String generateToken(User user) {
+        Instant now = Instant.now();
+        String userId = String.valueOf(user.getId());
+
+        return Jwts.builder()
+                .subject(userId)
+                .claim("role", user.getRole())
+                .issuedAt(Date.from(now))
+                .expiration(Date.from(now.plus(1, ChronoUnit.HOURS)))
+                .signWith(secretKey)
+                .compact();
+    }
+
+    @Override
+    public UUID extractUserId(String token) {
+        return null;
+    }
+
+    @Override
+    public boolean isTokenValid(String token, UserDetails userDetails) {
+        return false;
+    }
+
+    @PostConstruct
+    void init() {
+        this.secretKey = Keys.hmacShaKeyFor(Decoders.BASE64.decode(secret));
+    }
+}
